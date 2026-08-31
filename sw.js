04c3334ef108020fc669df5ad9fb3e29975ccc8f
Ravi627 <chandakravi627@gmail.com>
2026-08-31 18:18:22 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,27 @@
-const CACHE_NAME = 'prod-tracker-v1';
-const ASSETS = ['./', './index.html', './app.js', './manifest.json'];
+const CACHE_NAME = 'prod-pwa-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icon.svg'
+];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+self.addEventListener('fetch', e => {
+  // Let Google Apps Script API calls bypass Service Worker cache
+  if (e.request.url.includes('script.google.com')) {
+    e.respondWith(fetch(e.request));
+    return;
+  }
+
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
 });
